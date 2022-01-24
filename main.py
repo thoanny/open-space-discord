c35e924c8bb70a262b5cc3619f99ca925f7a90bd
Anthony Destenay <anthony.destenay@gmail.com>
2022-01-24 18:03:09 +0100

    Ajout de la commande !sync

--- main.py
+++ main.py
@@ -1,16 +1,69 @@
-# This is a sample Python script.
+import os
+import discord
+import psycopg2
+import json
+import requests
+from dotenv import dotenv_values
+from psycopg2.extras import RealDictCursor
+from discord.ext import commands
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+config = {
+    **dotenv_values(".env"),
+    **os.environ,
+}
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+MESSAGE_DELETE_AFTER: int = 5
+
+conn = psycopg2.connect(
+    "host=%s dbname=%s user=%s password=%s" % (
+        config['DB_HOST'], config['DB_BASE'],
+        config['DB_USER'], config['DB_PASS']
+    )
+)
+db = conn.cursor(cursor_factory=RealDictCursor)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@bot.command(name='sync')
+async def twitch_sync(ctx, arg):
+    role = discord.utils.get(ctx.message.guild.roles, name="Certifié")
+    user = ctx.message.author
+
+    if role not in user.roles:
+        sync = requests.get(config['SYNC_API'] + arg)
+        data = json.loads(sync.text)
+
+        if 'error' in data:
+            await ctx.message.channel.send('[Erreur] '+data['error'], delete_after=MESSAGE_DELETE_AFTER*2)
+        else:
+            await user.add_roles(role)
+            await user.edit(nick=data['success'])
+            await ctx.message.channel.send('Bravo, tu es certifié·e !', delete_after=MESSAGE_DELETE_AFTER)
+
+    else:
+        await ctx.message.channel.send('Tu as déjà le rôle certifié !', delete_after=MESSAGE_DELETE_AFTER)
+
+    await ctx.message.delete()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@twitch_sync.error
+async def on_twitch_sync_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        message = 'La commande est `!sync [token]` en remplaçant [token] par ton jeton d\'identification.'
+    else:
+        message = f'[Erreur] {error}'
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    await ctx.send(message, delete_after=MESSAGE_DELETE_AFTER*2)
+    await ctx.message.delete()
+
+
+@bot.event
+async def on_ready():
+    print('L\'agent d\'accueil est opérationnel !')
+
+
+bot.run(config['DISCORD_BOT_TOKEN'])
